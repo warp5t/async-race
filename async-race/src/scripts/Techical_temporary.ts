f92857a0c393f8523cefd80dcda0e284ba65900c
warp5t <dmitrij.didencko@yandex.by>
2023-08-31 16:32:46 +0300

    feat: implement createWinner, deleteWinner, updateWinner

--- async-race/src/scripts/Techical_temporary.ts
+++ async-race/src/scripts/Techical_temporary.ts
@@ -5,7 +5,8 @@
 // import { countPages } from "./switchPages";
 // import { remainUFO } from "./createTrack";
 
-import { updateSheep, getSheeps, getSheep, createSheep, deleteSheep, stpStrtDriveEngine, getWinners } from "./serverRequest";
+import { updateSheep, getSheeps, getSheep, createSheep, deleteSheep, stpStrtDriveEngine,
+getWinners, getWinner, createWinner, deleteWinner, updateWinner } from "./serverRequest";
 
 const wrap = document.querySelector('.wrap') as HTMLDivElement;
 const  techicalBtn = document.createElement('button') as HTMLButtonElement;
@@ -16,28 +17,42 @@ const  techicalBtn_3 = document.createElement('button') as HTMLButtonElement;
 const  techicalBtn_4 = document.createElement('button') as HTMLButtonElement;
 const  techicalBtn_5 = document.createElement('button') as HTMLButtonElement;
 const  techicalBtn_6 = document.createElement('button') as HTMLButtonElement;
-const arrTechBtn = [techicalBtn, techicalBtn_0, techicalBtn_1, techicalBtn_2, techicalBtn_3, techicalBtn_4, techicalBtn_5, techicalBtn_6]
+const  techicalBtn_7 = document.createElement('button') as HTMLButtonElement;
+const  techicalBtn_8 = document.createElement('button') as HTMLButtonElement;
+const  techicalBtn_9 = document.createElement('button') as HTMLButtonElement;
+const  techicalBtn_10 = document.createElement('button') as HTMLButtonElement;
+const arrTechBtn = [techicalBtn, techicalBtn_0, techicalBtn_1, techicalBtn_2,
+techicalBtn_3, techicalBtn_4, techicalBtn_5, techicalBtn_6, techicalBtn_7,
+techicalBtn_8,techicalBtn_9, techicalBtn_10];
+
 arrTechBtn.forEach(element => {
   element.style.height = '70px';
   element.style.width = '70px';
   wrap.append(element);
 });
+
 let switcher = true;
+
 techicalBtn.addEventListener('click', () =>{
   getSheeps()
 })
+techicalBtn.innerText = 'getSheeps';
 techicalBtn_0.addEventListener('click', () => {
   getSheep(1)
 })
+techicalBtn_0.innerText = 'getSheep';
 techicalBtn_1.addEventListener('click', () => {
   updateSheep(1,'Rusted Tank', '#ff00ff')
 })
+techicalBtn_1.innerText = 'updateSheep';
 techicalBtn_2.addEventListener('click', () => {
   createSheep('Super Furter', '#ff00ff')
 })
+techicalBtn_2.innerText = 'createSheep';
 techicalBtn_3.addEventListener('click', () => {
   deleteSheep(6)
 })
+techicalBtn_3.innerText = 'deleteSheep';
 techicalBtn_4.addEventListener('click', () => {
   if(!switcher) {
     stpStrtDriveEngine(3,'started')
@@ -47,9 +62,29 @@ techicalBtn_4.addEventListener('click', () => {
     switcher = false;
   }
 })
+techicalBtn_4.innerText = 'stpStrtDriveEngine';
 techicalBtn_5.addEventListener('click', () => {
   if(switcher) stpStrtDriveEngine(3, 'drive');
 })
+techicalBtn_5.innerText = 'stpStrtDriveEngine - drive';
 techicalBtn_6.addEventListener('click', () => {
   getWinners()
 })
+techicalBtn_6.innerText = 'getWinners';
+techicalBtn_7.addEventListener('click', () => {
+  getWinner(1)
+})
+techicalBtn_7.innerText = 'getWinner';
+techicalBtn_8.addEventListener('click', () => {
+  createWinner(3,1,20)
+})
+techicalBtn_8.innerText = 'createWinner';
+techicalBtn_9.addEventListener('click', () => {
+  deleteWinner(3)
+})
+techicalBtn_9.innerText = 'deleteWinner';
+
+techicalBtn_10.addEventListener('click', () => {
+  updateWinner(3, 3, 33)
+})
+techicalBtn_10.innerText = 'updateWinner';
--- async-race/src/scripts/serverRequest.ts
+++ async-race/src/scripts/serverRequest.ts
@@ -99,3 +99,81 @@ export async function getWinners() {
   .then(data => console.log(data, typeof(data)))
   .catch(error => console.error(error))
 }
+
+export async function getWinner(id:number) {
+  await fetch(`http://127.0.0.1:3000/winners/${id}`, {
+    method: 'GET',
+  })
+  .then(data => data.json())
+  .then(data => console.log(data, typeof(data)))
+  .catch(error => console.error(error))
+}
+
+export async function createWinner(id:number, wins:number, time:number) {
+
+  const winnerSheep =  {
+    id: id,
+    wins: wins,
+    time: time
+  }
+
+  await fetch(`http://127.0.0.1:3000/winners`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(winnerSheep),
+  })
+  .then(response => {
+    if (response.ok) {
+      console.log('Winner created successfully');
+    } else {
+      console.error('Failed to create winner');
+    }
+  })
+  .catch(error => {
+    console.error('An error occurred:', error);
+  });
+}
+
+export async function deleteWinner(id:number) {
+  await fetch(`http://127.0.0.1:3000/winners/${id}`, {
+    method: 'DELETE',
+  })
+  .then((response) => {
+    if(response.ok) {
+      console.log('Sheep deleted succesfully')
+    } else {
+      console.error('Failed to delete sheep')
+    }
+  })
+    .catch((error) => {
+      console.error('An error occurred:', error)
+  })
+}
+
+export async function updateWinner(id:number, wins:number, time:number) {
+
+  const updateSheep = {
+    wins: wins,
+    time: time,
+  };
+
+  await fetch(`http://127.0.0.1:3000/winners/${id}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(updateSheep),
+  })
+  .then((response) => {
+    if (response.ok) {
+      console.log('Winner updated successfully')
+    } else {
+      console.error('Failed to update winner')
+    }
+  })
+  .catch(error => {
+    console.error('An error occurred:', error)
+  })
+}
